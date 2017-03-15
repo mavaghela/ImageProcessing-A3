54d629475175b9b1c2331fc055d7c48f934f10d6
Mansi Vaghela <mansipv@gmail.com>
2017-03-14 20:02:19 -0400

    creates an affine matrix now

---
+++ affineMatrix.m
@@ -0,0 +1,27 @@
+% Solve affine transformation between features using top 3 corresp. from a2q2b
+function out = a2q2c(keypoints)
+ 
+% entire reference sets
+fRef = keypoints('fRef');
+fTest = keypoints('fTest');
+ 
+% keypoint indices
+rInd = keypoints('rInd');
+tInd = keypoints('tInd');
+ 
+% point matrices [x1, x2, x3; y1, y2, y3]
+pRef = [fRef(1:2, rInd(1):rInd(1)), fRef(1:2, rInd(2):rInd(2)), fRef(1:2, rInd(3):rInd(3))];
+pTest = [fTest(1:2, tInd(1):tInd(1)), fTest(1:2, tInd(2):tInd(2)), fTest(1:2, tInd(3):tInd(3))];
+ 
+% We are using only 3 keypoints, so we will get the affine transformation a = P^-1 * P'
+P = []
+for i = 1:3
+    r1 = [pRef(1, i), pRef(2, i), 0, 0, 1, 0];
+    r2 = [0, 0, pRef(1, i), pRef(2, i), 0, 1];
+    P = [P; r1; r2];
+end
+ 
+P_prime = [pTest(1,1); pTest(2,1); pTest(1,2); pTest(2,2); pTest(1,3); pTest(2,3)];
+ 
+out = inv(P)*P_prime;
+end
--- q2.m
+++ q2.m
@@ -7,6 +7,43 @@ images=['data/01.jpg'; 'data/02.jpg'; 'data/03.jpg'; 'data/04.jpg'; 'data/05.jpg
 images = string(images);
 %for i = 1:10
    % threshold is 0.8 
-   matches = a2q2b('./data/toy.jpg', './data/01.jpg'); 
+% matches = a2q2b('./data/toy.jpg', './data/01.jpg'); 
+
+imTest = imread('./data/01.jpg');
+imgTest = rgb2gray(imTest);
+
+[fRef, dRef] = sift(im2double(toyImg));
+[fTest, dTest] = sift(im2double(imgTest));
+d = dist2(dRef.', dTest.');
+[n, m] = size(d);
+thresh = 0.8;
+
+counter = 1;
+[d_sorted, d_index] = sort(d, 2);
+for i = 1:n
+    v_closest = d_index(i,1);
+    v_second = d_index(i,2);
+    ratio = d_sorted(i,1)./d_sorted(i,2);
+    if ratio < thresh
+        matches(counter, 1) = i;
+        matches(counter, 2) = v_closest;
+        counter = counter + 1;
+    end
+end
+
+[numOfMatches, height] = size(matches);  
+
+   % generating 3 random matches
+   X = randi(numOfMatches);
+   Y = 0;
+   Z = 0; 
    
+   while (X == Y || X == Z || Y == Z)
+      Y = randi(numOfMatches);
+      Z = randi(numOfMatches);
+   end
+   
+  someMap = containers.Map({'fRef', 'fTest', 'rInd', 'tInd'}, {fRef, fTest, [matches(X,1), matches(Y,1), matches(Z,1)],[matches(X,2), matches(Y,2), matches(Z,2)]});
+  P = affineMatrix(someMap); 
+  P
 %end
